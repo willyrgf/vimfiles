63a69d982d468851f64e9743d00d7ec46c00711d
Willy Romao <willyr.goncalves@gmail.com>
2014-03-18 19:42:24 -0300

    Primeiro commit para o repo.

--- installVim.sh
+++ installVim.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 
-# Gerenciador de pacotes, se esta setado no array ele sera usado.
-# Sempre use:
-# packageManager=("$(which gerenciador)" "arg de instalacao" 
-#                   "yes automatico do gerenciador"), exemplos:
-# packageManager=("$(which apt-qet)" "install" "-y")
-# packageManager=("$(which yum)" "install" "-y")
-# packageManager=("$(which zypper)" "install" "-y")
-# Ou, packageManager=(""), para que ele nao seja usado.
-#
 packageManager=("$(which apt-get)" "install" "-y")
 
 sudo ${packageManager[@]} vim vim-gtk gvim git
 
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 
-git clone https://github.com/willyrgf/vimfiles ~/
+git clone https://github.com/willyrgf/vimfiles ~/ || exit
+
+mv ~/gvimrc ~/.gvimrc || exit
+mv ~/vimrc ~/.vimrc || exit
+
+vim +:BundleClean +q +q
+vim +:BundleInstall +q +q
+
 
 
