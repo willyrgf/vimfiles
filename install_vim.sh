ce45b874209cddb3b03fe888f3dac076cc82e311
Willy Romão <willyr.goncalves@gmail.com>
2014-09-24 15:25:45 -0300

    Update install_vim
    Move -> link symbolic

--- install_vim.sh
+++ install_vim.sh
@@ -8,7 +8,7 @@ git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 
 git pull
 
-mv -f vimrc ~/.vimrc || exit
+ln -s vimrc ~/.vimrc || exit
 
 vim +:BundleClean +q +q
 vim +:BundleInstall +q +q
