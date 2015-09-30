524f0021c7fc997edc8ce34bbed24383fb881b74
Willy Romao <willyr.goncalves@gmail.com>
2015-09-30 15:39:55 -0300

    check if vim is installed

--- install_vimrc.sh
+++ install_vimrc.sh
@@ -4,6 +4,12 @@
 # @email willyr.goncalves@gmail.com
 #
 
+# verifica se o vim esta instalado
+if [[ $(which vim) ]]; then
+  echo "Instale o vim, burro."
+  exit 1
+fi
+
 # Faz o clone do repositorio do vundle, o gerenciador de plugins
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 
